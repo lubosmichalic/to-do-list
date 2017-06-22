85f39dfd0906ce55c7bf078c63b435a2b0797548
Dom <dom.mcdonnell@gmail.com>
2017-06-22 17:00:34 +0100

    update and fix single note view test

--- tests/single-note-view-test.js
+++ tests/single-note-view-test.js
@@ -1,6 +1,6 @@
 function testSingleNoteView() {
   console.log("hello")
-  var note = new Note("String");
+  var note = "String";
   var singleNote = new SingleNoteView(note);
   assert.isEqual(singleNote.printHTML(), "<div>String</div>");
 };
